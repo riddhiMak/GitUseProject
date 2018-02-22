02c971903960adc24cf3279d10f1720bb0311b55
Riddhi Makvana <riddhimakvana30@gmail.com>
2018-02-22 15:52:03 +0530

    Initial Commit

---
+++ GitUseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseProject
+//
+//  Created by Riddhi Makvana on 22/02/18.
+//  Copyright © 2018 Riddhi Makvana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
